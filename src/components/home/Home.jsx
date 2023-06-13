21c0dacdf0336c69e5e6e82f30606f76c701dab2
js_heo <js_heo@hancomum.com>
2023-06-13 17:31:57 +0900

    [수정] 수정 백업

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,16 @@
-
+import styled from 'styled-components'
 
 const Home = () => {
 
-  return <div className="text-4xl text-white">Home</div>
+  return (
+    <ContentsBox className="flex flex-row mt-6 h-[44rem] bg-zinc-800 w-screen overflow-hidden text-4xl text-white">
+      Home
+    </ContentsBox>
+  )
 }
+
+const ContentsBox = styled.div`
+  position : relative;
+`
 
 export default Home;
--- src/components/page/components/AppHeader.jsx
+++ src/components/page/components/AppHeader.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import logo from '/src/assets/icons/logo.png'
 import styled from 'styled-components'
+import '../style/header.module.css'
 
 import { AdjustmentsHorizontalIcon, ChartBarIcon, CogIcon, HomeIcon, TableCellsIcon } from '@heroicons/react/24/outline'
 
@@ -10,29 +11,43 @@ const AppHeader = ({ pageFlag, setPageFlag }) => {
   const [version, setVersion] = useState('v0.0.1');
   const [title, setTitle] = useState('HUBPRO ( Gas Monitoring System )') // HUBPRO ( Gas Monitoring System )
 
+  const icons = [
+    { IconComponent: HomeIcon, flag: 1 },
+    { IconComponent: AdjustmentsHorizontalIcon, flag: 2 },
+    { IconComponent: TableCellsIcon, flag: 3 },
+    { IconComponent: ChartBarIcon, flag: 4 },
+    { IconComponent: CogIcon, flag: 5 },
+  ];
+
   useEffect(() => {
     getIpData(setIP) // IP 주소 가져오기
     getTitleName(setTitle, setVersion)
   }, []);
 
   return (
-    <HeaderContainer className="flex flex-row space-x-10 items-center w-screen h-24 bg-zinc-800 text-center text-yellow-400 text-5xl font-bold">
+    <HeaderContainer className="flex flex-row space-x-10 items-center w-screen h-24 bg-zinc-800 text-center text-yellow-400 text-4xl font-bold">
+
       <div className="w-1/12 ml-4 min-h-10">
         <img className="absolute left-5 top-3 w-40" src={logo} onClick={() => setPageFlag(1)}/>
       </div>
+
       <div className="w-1/12 min-h-10">
         <div className="text-sm font-normal text-zinc-500">{ip}<br></br>( { version } )</div>
       </div>
-      <div className="w-7/12 min-h-10">{title}</div>
-      <div className="flex flex-row justify-end w-3/12 min-h-10 text-sm font-normal text-white">
 
-        <HomeIcon className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === 1 && 'text-yellow-400'}`} onClick={()=>setPageFlag(1)} />
-        <AdjustmentsHorizontalIcon className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === 2 && 'text-yellow-400'}`} onClick={()=>setPageFlag(2)}/>
-        <TableCellsIcon className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === 3 && 'text-yellow-400'}`} onClick={()=>setPageFlag(3)}/>
-        <ChartBarIcon className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === 4 && 'text-yellow-400'}`} onClick={()=>setPageFlag(4)}/>
-        <CogIcon className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === 5 && 'text-yellow-400'}`} onClick={()=>setPageFlag(5)}/>
-        
+      <div className="w-7/12 min-h-10">{title}</div>
+
+      <div className="flex flex-row justify-end w-3/12 min-h-10 text-sm font-normal text-white">
+        {icons.map(({ IconComponent, flag }) => (
+          // 라우팅 아이콘 그룹
+          <IconComponent 
+            className={`flex flex-col w-12 h-12 mx-4 ${pageFlag === flag ? 'text-yellow-400' : ''}`} 
+            onClick={() => setPageFlag(flag)}
+            key={flag}
+          />
+        ))}
       </div>
+
     </HeaderContainer>
   )
 }
